92134063018b1bbd687db526ecb3dcc8b3390128
MSakamaki <daigouzigai5@gmail.com>
2014-02-16 11:46:33 +0900

    server seed

---
+++ app.js
@@ -0,0 +1,41 @@
+/* server define */
+var restServer = require('restify').createServer();
+restServer.listen(8081, function() {
+	console.log('listening at ', restServer.name, restServer.url);
+});
+
+
+/************************  REST Server *******************************/
+restServer.use(
+	/* cross Origin Option */
+	function crossOrigin(req,res,next){
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		return next();
+	}
+);
+
+
+/* rest function */
+var rPost = function(req,res,next){
+	res.send(201,"post action!");
+	return next();
+}
+var rSend = function(req,res,next){
+	console.log('/get', req.params.XxxX);
+	var param = req.params.XxxX;
+	try{
+		res.send(JSON.stringify({ p : param }));
+	}catch(e){ 
+		console.log('err:',e);
+		res.send(JSON.stringify({err:"505 server error!"}));
+	}
+	//return next();
+}
+
+/* rest URI */
+restServer.put('/get', rSend);
+restServer.get('/get/:XxxX', rSend);
+restServer.head('/get/:XxxX',rSend);
+restServer.del('/get/:XxxX', rSend);
+restServer.post('/hello', rPost);
